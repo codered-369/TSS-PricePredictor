51b335f67424f91903cf01314a6c4e5cab0b7c5b
Vishwanath Gouda <121312091+codered-369@users.noreply.github.com>
2026-07-01 14:25:33 +0530

    Revise keywords in layout metadata
    Updated keywords in metadata for better SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "TSS Arecanut Price Sirsi Today | Daily Market Rates & 7-Day Forecast",
   description: "Get the exact, 100% accurate daily market prices for TSS Sirsi Arecanut (Rashi, Chali, Kempu Gotu, Pepper). Check live APMC rates and our exclusive AI-powered 7-day price prediction to know exactly when to sell your crop.",
-  keywords: ["TSS Sirsi", "Arecanut price Sirsi today", "Sirsi supari price", "TSS market rate", "Sirsi APMC arecanut", "betel nut price Sirsi", "Rashi arecanut price", "Chali kempu price", "Totagars Cooperative Sale Society", "kalumenasu price", "black pepper price Sirsi", "TSS black pepper", "Sirsi adike price", "arecanut prediction"],
+  keywords: ["TSS Sirsi", "Arecanut price Sirsi today", "Sirsi supari price", "TSS market rate", "Sirsi APMC arecanut", "Sirsi","Adike","Sirsi Adike rate","TSS","Tss","betel nut price Sirsi", "Rashi arecanut price", "Chali kempu price", "Totagars Cooperative Sale Society", "kalumenasu price", "black pepper price Sirsi", "TSS black pepper", "Sirsi adike price", "arecanut prediction"],
   openGraph: {
     title: "TSS Arecanut Price Sirsi | Live Rates & AI Forecast",
     description: "Check today's true TSS Sirsi Arecanut prices. View 7-day AI predictions for Rashi, Kempu Gotu, Chali, and Pepper to maximize your profits.",
